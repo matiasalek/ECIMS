83a7904bd95a4a9f2ed5a2ecaaf37e7c0ae131e2
Matias <matias.aleksandrowicz@gmail.com>
2025-03-06 00:19:03 -0300

    initial commit

---
+++ src/main/java/com/ECommerceIMS/ECIMS/EcimsApplication.java
@@ -0,0 +1,13 @@
+package com.ECommerceIMS.ECIMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ECommerceIMS/ECIMS/EcimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ECommerceIMS.ECIMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
