4b7628bc078dea5e566d3329a2e98026859811f5
Matias <matias.aleksandrowicz@gmail.com>
2025-03-15 17:59:47 -0300

    Added methods to OrderItemRepo'

--- src/main/java/com/ECommerceIMS/ECIMS/repository/OrderItemRepository.java
+++ src/main/java/com/ECommerceIMS/ECIMS/repository/OrderItemRepository.java
@@ -1,7 +1,13 @@
 package com.ECommerceIMS.ECIMS.repository;
 
 import com.ECommerceIMS.ECIMS.model.OrderItem;
+import com.ECommerceIMS.ECIMS.model.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrders_Order_id(Long order_id);
+    List<OrderItem> findByProducts_Product_id(Long product_id);
+    List<OrderItem> findByWarehouses_Warehouse_id(Long warehouse_id);
 }
