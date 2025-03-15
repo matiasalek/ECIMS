8c1ceeccf7051c3224c048079d36d51095626423
Matias <matias.aleksandrowicz@gmail.com>
2025-03-15 14:36:32 -0300

    Finished InventoryTransactionsDTO

--- src/main/java/com/ECommerceIMS/ECIMS/DTO/InventoryTransactionsDTO.java
+++ src/main/java/com/ECommerceIMS/ECIMS/DTO/InventoryTransactionsDTO.java
@@ -1,4 +1,62 @@
 package com.ECommerceIMS.ECIMS.DTO;
 
+import java.time.LocalDateTime;
+
 public class InventoryTransactionsDTO {
+    private Long transaction_id;
+    private long inventory_id;
+    private String transaction_type;
+    private int quantity;
+    private String notes;
+    private LocalDateTime created_at;
+
+    public InventoryTransactionsDTO () {}
+
+    public Long getTransaction_id() {
+        return transaction_id;
+    }
+
+    public void setTransaction_id(Long transaction_id) {
+        this.transaction_id = transaction_id;
+    }
+
+    public long getInventory_id() {
+        return inventory_id;
+    }
+
+    public void setInventory_id(long inventory_id) {
+        this.inventory_id = inventory_id;
+    }
+
+    public String getTransaction_type() {
+        return transaction_type;
+    }
+
+    public void setTransaction_type(String transaction_type) {
+        this.transaction_type = transaction_type;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
 }
